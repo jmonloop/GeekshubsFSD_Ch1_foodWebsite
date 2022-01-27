9f7e1842c4606fe78aac3754925deeabbaf1497a
jmonloop <monleonlopezjavier@gmail.com>
2022-01-27 17:02:09 +0100

    carousel slide bug fixed

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// var myCarousel1 = document.querySelector('#carouselMenu1');
+// var myCarousel2 = document.querySelector('#carouselMenu2');
+// var carousel1 = new bootstrap.Carousel(carouselMenu1);
+// var carousel2 = new bootstrap.Carousel(carouselMenu2);
+
+// console.log("🚀 ~ file: main.js ~ line 6 ~ carousel1", carousel1)
