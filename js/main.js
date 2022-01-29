a6c6f5de5a626da1b87ffffc568367a4ff87fe9d
jmonloop <monleonlopezjavier@gmail.com>
2022-01-29 21:32:25 +0100

    hide all collapse buttons added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
-// var myCarousel1 = document.querySelector('#carouselMenu1');
-// var myCarousel2 = document.querySelector('#carouselMenu2');
-// var carousel1 = new bootstrap.Carousel(carouselMenu1);
-// var carousel2 = new bootstrap.Carousel(carouselMenu2);
 
-// console.log("🚀 ~ file: main.js ~ line 6 ~ carousel1", carousel1)
+const bodyClick = document.querySelectorAll('.hideAll')
+
+const martes = document.getElementById('martes')
+const miercoles = document.getElementById('miercoles')
+const jueves = document.getElementById('jueves')
+const viernes = document.getElementById('viernes')
+const sabado = document.getElementById('sabado')
+
+const bsCollapse1 = new bootstrap.Collapse(martes)
+bodyClick.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse1.hide() })
+})
+const bsCollapse2 = new bootstrap.Collapse(miercoles)
+bodyClick.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse2.hide() })
+})
+const bsCollapse3 = new bootstrap.Collapse(jueves)
+bodyClick.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse3.hide() })
+})
+const bsCollapse4 = new bootstrap.Collapse(viernes)
+bodyClick.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse4.hide() })
+})
+const bsCollapse5 = new bootstrap.Collapse(sabado)
+bodyClick.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse5.hide() })
+})
+
+
+
+
+// const collapseHide = (master, elmnt) => {
+//     const masterClick = document.querySelectorAll(master)
+//     const slaveHidden = document.getElementById(elmnt)
+//     const bsCollapse = new bootstrap.Collapse(slaveHidden)
+//     masterClick.forEach((l) => {
+//         l.addEventListener('click', () => { bsCollapse.hide() })
+//     })
+// }
